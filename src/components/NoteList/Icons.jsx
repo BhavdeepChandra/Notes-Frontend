730fae10d005e2018e7045b1ec04dbe0058fc95c
BhavdeepChandra <bhavdeepchandra@gmail.com>
2026-06-19 21:41:22 +0530

    Add an Edit Button

--- src/components/NoteList/Icons.jsx
+++ src/components/NoteList/Icons.jsx
@@ -83,3 +83,23 @@ export function TrashIcon(props) {
     </svg>
   );
 }
+
+export function EditIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="16"
+      height="16"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 20h9"></path>
+      <path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z"></path>
+    </svg>
+  );
+}
