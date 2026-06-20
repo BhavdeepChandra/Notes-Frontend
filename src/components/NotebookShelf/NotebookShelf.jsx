128e34557249fe8afc628f858902bcce307e0d0e
BhavdeepChandra <bhavdeepchandra@gmail.com>
2026-06-20 23:20:48 +0530

    Create a component for NotebookShelf

---
+++ src/components/NotebookShelf/NotebookShelf.jsx
@@ -0,0 +1,10 @@
+import './NotebookShelf.css';
+
+export default function NotebookShelf() {
+  return (
+    <div className="notebookshelf-container">
+      <h2 className="notebookshelf-title">NotebookShelf</h2>
+      <p className="notebookhelf-placeholder">NotebookShelf component content will go here.</p>
+    </div>
+  );
+}
